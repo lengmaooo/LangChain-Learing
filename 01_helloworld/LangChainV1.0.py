2477895b3908200e9948b30a9a587a316aefdeb6
Niko <danny@sinokru.com>
2026-03-10 10:51:33 +0800

    取消apikey

--- 01_helloworld/LangChainV1.0.py
+++ 01_helloworld/LangChainV1.0.py
@@ -1,8 +1,6 @@
 # LangChain1.0+版本使用方式,目前主流
 
 # 1.导入依赖
-import os
-from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 
 # 2.实例化模型
@@ -10,7 +8,7 @@ from langchain.chat_models import init_chat_model
 model = init_chat_model(
     model="qwen3.5-plus",
     model_provider="openai",
-    api_key="sk-cf40bc548a354754889cfab2c48b033b",
+    api_key="XXXXX",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
